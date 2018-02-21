3f5dc0de3d4310e3680455f1312e42abd6b3b10d
stejstin <stejstin@ut.utm.edu>
2018-02-21 10:38:27 -0600

    Added sms functionality and morse beeps!

--- MorseCode/app/src/main/java/com/example/user/morsecode/MainActivity.kt
+++ MorseCode/app/src/main/java/com/example/user/morsecode/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.user.morsecode
 
+import android.Manifest.permission.SEND_SMS
 import android.app.Activity
 import android.app.PendingIntent.getActivity
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.media.AudioFormat
 import android.media.AudioManager
 import android.media.AudioTrack
 import android.os.Bundle
+import android.preference.PreferenceManager.getDefaultSharedPreferences
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.telephony.SmsManager
 import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import android.view.Menu
@@ -27,19 +31,31 @@ import java.io.InputStream
 import java.lang.Math.round
 import java.util.*
 import kotlin.concurrent.timerTask
+import android.widget.Toast
+import java.nio.file.Files.size
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.pm.PackageManager
+import java.util.jar.Manifest
+
 
 val SAMPLE_RATE = 44100;
 
 class MainActivity : AppCompatActivity() {
+    var prefs: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        prefs = getDefaultSharedPreferences(this.applicationContext)
+
+
+        val morsePitch = prefs!!.getString("morse_pitch", "550").toInt()
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            sendSMS("7312342862", mTextView.text.toString())
         }
         mTextView.movementMethod = ScrollingMovementMethod()
 
@@ -58,9 +74,77 @@ class MainActivity : AppCompatActivity() {
             translate()
         }
         playButton.setOnClickListener{ view ->
+                playString(mTextView.text.toString())
 
         }
+
+
+
     }
+
+
+    private fun sendSMS(phoneNumber:String, message:String) {
+        val sentPendingIntents = ArrayList<PendingIntent>()
+        val deliveredPendingIntents = ArrayList<PendingIntent>()
+
+        val sentPI = PendingIntent.getBroadcast( this, 0,
+                Intent(this, SmsSentReceiver::class.java), 0)
+        val deliveredPI = PendingIntent.getBroadcast(this , 0,
+                Intent(this , SmsDeliveredReceiver::class.java), 0)
+
+        val PERMISSION_REQUEST_CODE = 1
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M)
+        {
+            if ((checkSelfPermission(SEND_SMS) === PackageManager.PERMISSION_DENIED))
+            {
+                Log.d("permission", "permission denied to SEND_SMS - requesting it")
+                val permissions = arrayOf<String>(SEND_SMS)
+                requestPermissions(permissions, PERMISSION_REQUEST_CODE)
+            }
+        }
+
+
+        try{
+            val sms = SmsManager.getDefault()
+            val mSMSMessage = sms.divideMessage(message)
+            for (i in 0 until mSMSMessage.size)
+            {
+                sentPendingIntents.add(i, sentPI)
+                deliveredPendingIntents.add(i, deliveredPI)
+            }
+            sms.sendMultipartTextMessage(phoneNumber, null, mSMSMessage,
+                    sentPendingIntents, deliveredPendingIntents)
+        }
+        catch (e:Exception) {
+            e.printStackTrace()
+            Toast.makeText(getBaseContext(), "SMS sending failed...", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    class SmsDeliveredReceiver:BroadcastReceiver() {
+        override fun onReceive(context:Context, arg1:Intent) {
+            when (getResultCode()) {
+                Activity.RESULT_OK -> Toast.makeText(context, "SMS delivered", Toast.LENGTH_SHORT).show()
+                Activity.RESULT_CANCELED -> Toast.makeText(context, "SMS not delivered", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    class SmsSentReceiver:BroadcastReceiver() {
+        override fun onReceive(context:Context, arg1:Intent) {
+            when (getResultCode()) {
+                Activity.RESULT_OK -> Toast.makeText(context, "SMS Sent", Toast.LENGTH_SHORT).show()
+                SmsManager.RESULT_ERROR_GENERIC_FAILURE -> Toast.makeText(context, "SMS generic failure", Toast.LENGTH_SHORT)
+                        .show()
+                SmsManager.RESULT_ERROR_NO_SERVICE -> Toast.makeText(context, "SMS no service", Toast.LENGTH_SHORT)
+                        .show()
+                SmsManager.RESULT_ERROR_NULL_PDU -> Toast.makeText(context, "SMS null PDU", Toast.LENGTH_SHORT).show()
+                SmsManager.RESULT_ERROR_RADIO_OFF -> Toast.makeText(context, "SMS radio off", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
 
     fun Activity.hideKeyboard() {
         hideKeyboard(if (currentFocus == null) View( this) else currentFocus)
@@ -248,6 +332,9 @@ class MainActivity : AppCompatActivity() {
         })
         mAudioTrack.play(); mAudioTrack.write(nBuffer, 0, minBufferSize)
     }
+
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
